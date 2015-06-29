8cf8e5cb2346c83decc8fb546359bcc8a6a8717a
Yuta Shimoda <yu06ta03@gmail.com>
2015-06-29 20:25:58 +0900

    make form and function to register user

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import pymysql.cursors
 
 app = Flask(__name__)
@@ -8,6 +8,21 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/user_form')
+def user_form():
+    return render_template('form.html')
+
+@app.route('/register_user', methods=["POST"])
+def register_user():
+    conn = connect_db()
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)", (request.form['email'], request.form['password']))
+
+    cur.close()
+    conn.commit()
+    conn.close()
+    return request.form['email'] + request.form['password']
 
 @app.route('/test_db')
 def test_db():
